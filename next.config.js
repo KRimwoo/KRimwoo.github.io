e2a31c0a93210ab237600a31e79d08ab320618a4
Woorim Kim <wrkimlimi@gmail.com>
2024-06-26 15:37:50 +0900

    fix: config's basePath

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withVanillaExtract = createVanillaExtractPlugin();
 
 const nextConfig = {
     output: "export",
-    basePath: "/",
+    basePath: "/portfolio",
     webpack: (config) => {
         config.module.rules.push({
             test: /\.svg$/,
